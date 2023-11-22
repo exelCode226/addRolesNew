cb982c5408245f9c1188e7a2b2bb0f143c09dff5
exelCode226 <141802107+exelCode226@users.noreply.github.com>
2023-11-21 21:27:32 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -35,14 +35,14 @@ app.use("/api", pedidosRouter);
 app.use("/api", programacionRouter);
 
 
-//if (process.env.NODE_ENV === "production") {
-  //const path = await import("path");
-  //app.use(express.static("client/dist"));
+if (process.env.NODE_ENV === "production") {
+  const path = await import("path");
+  app.use(express.static("client/dist"));
 
-  //app.get("*", (req, res) => {
-    //console.log(path.resolve("client", "dist", "index.html") );
-    //res.sendFile(path.resolve("client", "dist", "index.html"));
-  //});
-//}
+  app.get("*", (req, res) => {
+    console.log(path.resolve("client", "dist", "index.html") );
+    res.sendFile(path.resolve("client", "dist", "index.html"));
+  });
+}
 
 export default app;
